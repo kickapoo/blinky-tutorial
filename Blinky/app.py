5b873b65e75ec84a95c0a9302c8582cf5cf39e49
Stavros Anastasiadis <anastasiadis.st00@gmail.com>
2015-11-15 16:21:08 +0200

    Add initial endpoints

--- Blinky/app.py
+++ Blinky/app.py
@@ -2,9 +2,29 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api/root/')
 def root_api():
-    return "Initial flask setup"
+    return "List api endpoints"
+
+@app.route('/api/led/<int:pin>/on/')
+def set_led_on(pin):
+    return "Set led attach to {} pin ON".format(pin)
+
+@app.route('/api/led/<int:pin>/off/')
+def set_led_off(pin):
+    return "Set led attach to {} pin OFF".format(pin)
+
+@app.route('/api/led/status/')
+def led_status():
+    return "Response led status"
+
+@app.route('/api/login/')
+def login():
+    return "Login User"
+
+@app.route('/api/logout')
+def logout():
+    return "Logout User"
 
 if __name__ == '__main__':
     app.run(debug=True)
